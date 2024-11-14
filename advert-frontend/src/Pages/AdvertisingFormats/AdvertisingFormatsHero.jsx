eda60e0192418b29d9dc29b5f0c12ded52c2300f
Diana725 <wachira.wangari2019@students.jkuat.ac.ke>
2024-11-14 10:58:58 +0300

    changed .jpg for maiko_img_4&11 to .JPG because that's how they are appearing on the server

--- advert-frontend/src/Pages/AdvertisingFormats/AdvertisingFormatsHero.jsx
+++ advert-frontend/src/Pages/AdvertisingFormats/AdvertisingFormatsHero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import hero_image from "../../assets/images/maiko_img_4.jpg";
+import hero_image from "../../assets/images/maiko_img_4.JPG";
 import "./AdvertisingFormatsHero.css";
 import Navbar from "../../components/Navbar/Navbar";
 
